0797e1e36ad7b8646c33792e4dfefe8a2e4aa418
GermainMichaud <gmichaud.dev@gmail.com>
2021-06-25 18:11:57 +0200

    Test push vercel

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello Vite + React! On Vercel</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => setCount(count => count + 1)}>
             count is: {count}
           </button>
         </p>
@@ -19,12 +19,7 @@ function App() {
           Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
           {' | '}
